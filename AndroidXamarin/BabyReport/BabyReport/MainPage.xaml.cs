4514bc552698527e5c7ff2acd4e2d516dffbdd0c
Chris Grabosky <chris@grabosky.net>
2019-08-16 15:50:18 -0400

    support for volume of intake

--- AndroidXamarin/BabyReport/BabyReport/MainPage.xaml.cs
+++ AndroidXamarin/BabyReport/BabyReport/MainPage.xaml.cs
@@ -22,14 +22,18 @@ namespace BabyReport
             txt_title.GestureRecognizers.Add(new TapGestureRecognizer((arg1, arg2) => view_web.Source = (view_web.Source as UrlWebViewSource).Url ));
             btn_poo.GestureRecognizers.Add(new TapGestureRecognizer(btn_poo_Click));
             btn_pee.GestureRecognizers.Add(new TapGestureRecognizer(btn_pee_Click));
-            btn_form.GestureRecognizers.Add(new TapGestureRecognizer(btn_fed_Click));
-            btn_milk.GestureRecognizers.Add(new TapGestureRecognizer(btn_milk_Click));
+            btn_form.GestureRecognizers.Add(new TapGestureRecognizer(openFormula));
+            btn_milk.GestureRecognizers.Add(new TapGestureRecognizer(openMilk));
 
 
         }
 
         private async Task MakeRequest(Newtonsoft.Json.Linq.JObject jobj)
         {
+            stk_form.IsVisible = false;
+            stk_milk.IsVisible = false;
+            view_web.IsVisible = true;
+
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(AppConstants.newEventEndpoint);
             client.DefaultRequestHeaders.Accept.Clear();
@@ -47,22 +51,39 @@ namespace BabyReport
             view_web.Source = (view_web.Source as UrlWebViewSource).Url;
         }
 
-        private async void btn_poo_Click(View arg1, object arg2)
+        private void openFormula(View arg1, object arg2)
+        {
+            stk_form.IsVisible = true;
+            stk_milk.IsVisible = false;
+            view_web.IsVisible = false;
+        }
+
+        private void openMilk(View arg1, object arg2)
+        {
+            stk_form.IsVisible = false;
+            stk_milk.IsVisible = true;
+            view_web.IsVisible = false;
+        }
+
+        private async void btnSaveMilk_Clicked(object sender, System.EventArgs e)
         {
             Newtonsoft.Json.Linq.JObject jobj = new Newtonsoft.Json.Linq.JObject();
-            jobj.Add("eventType", "diaper");
-            jobj.Add("eventSubType", "poo");
+            jobj.Add("eventType", "bottle");
+            jobj.Add("eventSubType", "milk");
+            double val = Convert.ToDouble(txt_milk.Text);
+            jobj.Add("qty", val);
             await MakeRequest(jobj);
         }
 
-        private async void btn_fed_Click(View arg1, object arg2)
+        private async void btnSaveFormula_Clicked(object sender, System.EventArgs e)
         {
             Newtonsoft.Json.Linq.JObject jobj = new Newtonsoft.Json.Linq.JObject();
             jobj.Add("eventType", "bottle");
             jobj.Add("eventSubType", "formula");
+            double val = Convert.ToDouble(txt_form.Text);
+            jobj.Add("qty", val);
             await MakeRequest(jobj);
         }
-
 
         private async void btn_pee_Click(View arg1, object arg2)
         {
@@ -72,11 +93,11 @@ namespace BabyReport
             await MakeRequest(jobj);
         }
 
-        private async void btn_milk_Click(View arg1, object arg2)
+        private async void btn_poo_Click(View arg1, object arg2)
         {
             Newtonsoft.Json.Linq.JObject jobj = new Newtonsoft.Json.Linq.JObject();
-            jobj.Add("eventType", "bottle");
-            jobj.Add("eventSubType", "milk");
+            jobj.Add("eventType", "diaper");
+            jobj.Add("eventSubType", "poo");
             await MakeRequest(jobj);
         }
 
