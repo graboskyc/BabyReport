cd9e135f2e2bcc65fc34d02dd413ba17030405c9
Chris Grabosky <chris@grabosky.net>
2019-08-15 22:02:16 -0400

    better formatting and color

--- PiUWP/BabyReport/BabyReport/MainPage.xaml.cs
+++ PiUWP/BabyReport/BabyReport/MainPage.xaml.cs
@@ -18,15 +18,43 @@ using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
 using System.Text;
+using Windows.UI.ViewManagement;
+using Windows.UI;
 
 namespace BabyReport
 {
     public sealed partial class MainPage : Page
     {
+        DispatcherTimer _disTimer;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            var view = ApplicationView.GetForCurrentView();
+
+            view.TitleBar.BackgroundColor = Color.FromArgb(255, 81, 191, 8);
+            view.TitleBar.ButtonBackgroundColor = Color.FromArgb(255, 81, 191, 8);
+            view.TitleBar.ButtonForegroundColor = Colors.White;
+            view.TitleBar.ButtonPressedForegroundColor = Color.FromArgb(255, 81, 191, 8);
+            view.TitleBar.ButtonPressedBackgroundColor = Colors.White;
+            view.TitleBar.ButtonHoverBackgroundColor = Colors.White;
+            view.TitleBar.ButtonHoverForegroundColor = Color.FromArgb(255, 81, 191, 8);
+
             view_web.Source = new Uri(AppConstants.chartsEmbed);
+
+            _disTimer = new DispatcherTimer();
+            _disTimer.Tick += _disTimer_Tick;
+            _disTimer.Interval = new TimeSpan(0, 0, AppConstants.refreshSecDelay);
+            _disTimer.Start();
+        }
+
+        private void _disTimer_Tick(object sender, object e)
+        {
+            view_web.Refresh();
+            _disTimer.Interval = new TimeSpan(0, 0, AppConstants.refreshSecDelay);
+            _disTimer.Start();
+            view_web.Refresh();
         }
 
         private async void view_web_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
@@ -50,6 +78,7 @@ namespace BabyReport
             response.EnsureSuccessStatusCode();
 
             string r = await response.Content.ReadAsStringAsync();
+            view_web.Refresh();
         }
 
         private async void btn_fed_Click(object sender, RoutedEventArgs e)
@@ -73,6 +102,14 @@ namespace BabyReport
             Newtonsoft.Json.Linq.JObject jobj = new Newtonsoft.Json.Linq.JObject();
             jobj.Add("eventType", "diaper");
             jobj.Add("eventSubType", "pee");
+            await MakeRequest(jobj);
+        }
+
+        private async void btn_milk_Click(object sender, RoutedEventArgs e)
+        {
+            Newtonsoft.Json.Linq.JObject jobj = new Newtonsoft.Json.Linq.JObject();
+            jobj.Add("eventType", "bottle");
+            jobj.Add("eventSubType", "milk");
             await MakeRequest(jobj);
         }
     }
