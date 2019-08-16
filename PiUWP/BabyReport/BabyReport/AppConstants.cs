e6148899e2d938a36a5f0fc6c50edb3aca8ca902
Chris Grabosky <chris@grabosky.net>
2019-08-15 20:33:22 -0400

    new constant

--- PiUWP/BabyReport/BabyReport/AppConstants.cs
+++ PiUWP/BabyReport/BabyReport/AppConstants.cs
@@ -3,5 +3,6 @@
     public static class AppConstants
     {
         public static string newEventEndpoint = "https://webhooks.mongodb-stitch.com/api/client/v2.0/app/babyreport-abc123/service/http/incoming_webhook/newEvent?secret=";
+        public static string chartsEmbedEndpoint = "";
     }
 }
