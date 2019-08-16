0f865ce321043a675f87933ac682ec1646256b65
Chris Grabosky <chris@grabosky.net>
2019-08-15 21:31:28 -0400

    commit

--- PiUWP/BabyReport/BabyReport/MainPage.xaml.cs
+++ PiUWP/BabyReport/BabyReport/MainPage.xaml.cs
@@ -12,19 +12,67 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Text;
 
 namespace BabyReport
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void view_web_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            await view_web.InvokeScriptAsync("setBody", new string[] { AppConstants.chartsEmbed });
+        }
+
+        private async Task MakeRequest(Newtonsoft.Json.Linq.JObject jobj)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(AppConstants.newEventEndpoint);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            string url = "newEvent?secret=" + AppConstants.eventEndpointSecret;
+
+            StringContent sc = new StringContent(jobj.ToString(), Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync(url, sc);
+
+            response.EnsureSuccessStatusCode();
+
+            string r = await response.Content.ReadAsStringAsync();
+        }
+
+        private async void btn_fed_Click(object sender, RoutedEventArgs e)
+        {
+            Newtonsoft.Json.Linq.JObject jobj = new Newtonsoft.Json.Linq.JObject();
+            jobj.Add("eventType", "bottle");
+            jobj.Add("eventSubType", "formula");
+            await MakeRequest(jobj);
+        }
+
+        private async void btn_poo_Click(object sender, RoutedEventArgs e)
+        {
+            Newtonsoft.Json.Linq.JObject jobj = new Newtonsoft.Json.Linq.JObject();
+            jobj.Add("eventType", "diaper");
+            jobj.Add("eventSubType", "poo");
+            await MakeRequest(jobj);
+        }
+
+        private async void btn_pee_Click(object sender, RoutedEventArgs e)
+        {
+            Newtonsoft.Json.Linq.JObject jobj = new Newtonsoft.Json.Linq.JObject();
+            jobj.Add("eventType", "diaper");
+            jobj.Add("eventSubType", "pee");
+            await MakeRequest(jobj);
         }
     }
 }
